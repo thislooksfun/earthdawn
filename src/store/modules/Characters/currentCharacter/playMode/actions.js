5d06bdb5207b345678a642d924675e4435aff211
thislooksfun <thislooksfun@repbot.org>
2019-09-07 03:07:23 -0500

    Calculate resulting wounds when dealing damage

--- src/store/modules/Characters/currentCharacter/playMode/actions.js
+++ src/store/modules/Characters/currentCharacter/playMode/actions.js
@@ -12,15 +12,20 @@ export default {
       armor: {
         physical: -1,
         mystic: 0,
-      }
+      },
+      health: {
+        woundThreshold: 1,
+      },
     }
     
     const armor = type == 'strain' ? 0 : char.armor[type];
     const amount = Math.max(dmg - armor, 0)
+    const wounds = Math.floor(amount / char.health.woundThreshold)
     
-    console.log(`Dealing ${amount} damage of type ${type}\n(raw damage: ${dmg}, relevant armor: ${armor})`)
+    console.log(`Dealing ${amount} damage of type ${type}\n(raw damage: ${dmg}, relevant armor: ${armor}, resulting wounds: ${wounds})`)
     
     commit('DEAL_DAMAGE', { uuid, amount });
+    commit('CAUSE_WOUNDS', { uuid, wounds });
   },
   recoverHealth({ commit }, amount) {
     commit('RECOVER_HEALTH', { uuid: routedUUID(), amount });

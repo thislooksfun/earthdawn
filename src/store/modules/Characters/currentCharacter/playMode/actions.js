2e49300d704ac99cf8a83052896a95403b27c8db
thislooksfun <thislooksfun@repbot.org>
2019-09-07 03:51:31 -0500

    Use decorated character instead of stub

--- src/store/modules/Characters/currentCharacter/playMode/actions.js
+++ src/store/modules/Characters/currentCharacter/playMode/actions.js
@@ -2,31 +2,28 @@
 // Example: setName -> ccSetName
 
 import router from '@/router'
+import decorate from '@/charDecorator'
+
 const routedUUID = () => router.currentRoute.params.uuid;
 
 export default {
-  dealDamage({ commit }, {type, dmg}) {
+  dealDamage({ commit, state }, { type, dmg }) {
     const uuid = routedUUID()
-    // const char = getters.charForUUID(uuid)
-    const char = {
-      armor: {
-        physical: -1,
-        mystic: 0,
-      },
-      health: {
-        woundThreshold: 1,
-      },
-    }
-    
-    const armor = type == 'strain' ? 0 : char.armor[type];
+    // NOTE: This creates a CLONE of the character object, so any setters here
+    // won't work (though they shouldn't be used outside of the mutator itself
+    // anyway).
+    const char = decorate(state.characters[uuid])
+
+    const armor = type == 'strain' ? 0 : char.characteristics.armor[type];
     const amount = Math.max(dmg - armor, 0)
     const wounds = Math.floor(amount / char.health.woundThreshold)
-    
+
     console.log(`Dealing ${amount} damage of type ${type}\n(raw damage: ${dmg}, relevant armor: ${armor}, resulting wounds: ${wounds})`)
-    
+
     commit('DEAL_DAMAGE', { uuid, amount });
     commit('CAUSE_WOUNDS', { uuid, wounds });
   },
+
   recoverHealth({ commit }, amount) {
     commit('RECOVER_HEALTH', { uuid: routedUUID(), amount });
   },

0bad19183382d4b3b23e52921831dfdbb143db24
thislooksfun <thislooksfun@repbot.org>
2019-09-07 03:51:05 -0500

    Flesh out health decorator

--- src/charDecorator/decorators/dynamic/health.js
+++ src/charDecorator/decorators/dynamic/health.js
@@ -1,30 +1,37 @@
 export default function (char) {
+  // Extract toughness attr
+  const { attrs: { tou } } = char;
+
+
+  // Inject getters into recoveryTests subsection
   char.health.recoveryTests = {
     ...char.health.recoveryTests,
 
     get perDay() {
       // Offset tou.val by 1 so the math works easier
-      return 1 + Math.floor((char.attrs.tou.val - 1) / 6);
+      return 1 + Math.floor((tou.val - 1) / 6) + char._effects._sum('recoveryTestsPerDay');
     },
     get step() {
-      return char.attrs.tou.step;
+      return tou.step + char._effects._sum('recoveryTestStep');
     }
   }
 
+
+  // Inject getters into main health section
   char.health = {
     ...char.health,
 
     get unconsciousnessThreshold() {
-      return char.attrs.tou.val * 2;
+      return tou.val * 2 + char._effects._sum('unconsciousnessThreshold');
     },
 
     get deathThreshold() {
-      return this.unconsciousnessThreshold + char.attrs.tou.step;
+      return this.unconsciousnessThreshold + tou.step_raw + char._effects._sum('deathThreshold');
     },
 
     get woundThreshold() {
       // Offset tou.val by 1 so the math works easier
-      return 3 + Math.floor((char.attrs.tou.val - 1) / 2);
+      return 3 + Math.floor((tou.val - 1) / 2) + char._effects._sum('woundThreshold');
     },
   }
 }

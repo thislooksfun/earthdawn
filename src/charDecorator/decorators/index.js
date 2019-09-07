71ae9ef67f0673830ddad016a305e1ec0e768872
thislooksfun <thislooksfun@repbot.org>
2019-09-07 03:00:38 -0500

    Add comments

--- src/charDecorator/decorators/index.js
+++ src/charDecorator/decorators/index.js
@@ -6,8 +6,13 @@ import dynamicDecorators from './dynamic'
 
 // Manually define order
 export default [
+  // Static decorators have no prereqs
   ...staticDecorators,
+  
+  // Effects and attributes require static decorators to be loaded first
   effects,
   attributes,
+  
+  // Dynamic decorators require effects, attributes, and static decorators
   ...dynamicDecorators,
 ]

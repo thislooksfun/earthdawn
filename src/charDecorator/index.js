f294ae465c65d04daaf2c602dae49e33c9f66cf6
thislooksfun <thislooksfun@repbot.org>
2019-09-10 10:19:10 -0500

    Add a reference to the stored character data

--- src/charDecorator/index.js
+++ src/charDecorator/index.js
@@ -6,6 +6,10 @@ export default function(char) {
   // Make a deep clone so we don't mess with the saved character information
   const c = cloneDeep(char);
 
+  // Store a reference to the underlying stored value, in case any decorators
+  // want to see the raw stored value before any decoration took place.
+  c._stored = char;
+  
   // Apply each decorator
   decorators.forEach(d => d(c));
 

da3ecdd4d3efdd72da02b941004a66439d9976b6
thislooksfun <thislooksfun@repbot.org>
2019-09-10 14:39:46 -0500

    Don't recurse into _stored
    Recursing into the `_stored` value results in duplicated effects.

--- src/charDecorator/decorators/effects.js
+++ src/charDecorator/decorators/effects.js
@@ -1,10 +1,10 @@
-function deepFindAll(obj, key, cb) {
+function deepFindAll(obj, except, key, cb) {
   for (const k in obj) {
     if (obj.hasOwnProperty(k)) {
       const el = obj[k];
       if (k == key) {
         cb(el);
-      } else if (typeof el === "object") {
+      } else if (typeof el === "object" && k != except) {
         deepFindAll(el, key, cb);
       }
     }
@@ -20,7 +20,7 @@ export default function(char) {
     },
   };
 
-  deepFindAll(char, "effects!", mods => {
+  deepFindAll(char, "_stored", "effects!", mods => {
     for (const mod in mods) {
       char._effects[mod] = char._effects[mod] || [];
       char._effects[mod].push(mods[mod]);

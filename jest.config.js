6ce5beac1e496684069b9a3894acfb2a2184d752
thislooksfun <thislooksfun@repbot.org>
2019-09-11 19:03:40 -0500

    Fix type
    Expected an array, got a string. Fixed now.

--- jest.config.js
+++ jest.config.js
@@ -13,7 +13,7 @@ module.exports = {
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
-  setupFilesAfterEnv: "./tests/setup/expect.js",
+  setupFilesAfterEnv: ["./tests/setup/expect.js"],
   snapshotSerializers: ["jest-serializer-vue"],
   testMatch: [
     "**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)",

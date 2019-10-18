e58305a0908d4c2f697f683dccbf44e8bc073ee9
thislooksfun <thislooksfun@repbot.org>
2019-10-17 23:41:09 -0500

    Organize spells by discipline

--- src/assets/earthdawn/spells/index.js
+++ src/assets/earthdawn/spells/index.js
@@ -7,14 +7,22 @@
 // BODY: This code pattern is repeated several times in the different asset
 // BODY: loaders. It should probably be broken out into a helper function.
 
-const files = require.context(".", false, /\.yml$/);
+const files = require.context(".", true, /\.yml$/);
 const spells = {};
 
 files.keys().forEach(key => {
   if (key.endsWith("__template__.yml")) return;
   const s = files(key);
+  const [discipline, circleStr] = key.split("/").slice(1, 3);
+
+  // Track which circle the spell is
+  s.circle = circleStr.slice(7);
+
+  // Ensure that the discipline category exists
+  spells[discipline] = spells[discipline] || {};
+
   // Add options for both singular and plural usage
-  spells[s.name] = s;
+  spells[discipline][s.name] = s;
 });
 
 export default spells;

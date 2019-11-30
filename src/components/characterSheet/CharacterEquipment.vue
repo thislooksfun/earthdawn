9b3bc3e1ba0800d8f6fedf7f9823ba7058131f1a
thislooksfun <thislooksfun@repbot.org>
2019-11-30 10:43:48 -0600

    Add Equipment panel

---
+++ src/components/characterSheet/CharacterEquipment.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="character-equipment">
+    <center><h2>Equipment</h2></center>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Count</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(count, name) in equipment" :key="name">
+          <td>{{ name }}</td>
+          <td>{{ count }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import decorate from "@/charDecorator";
+
+export default {
+  props: {
+    uuid: {
+      type: String,
+      default: null,
+    },
+  },
+  data() {
+    const char = this.$store.state.Characters.characters[this.uuid];
+    return { char };
+  },
+  computed: {
+    dChar() {
+      return decorate(this.char);
+    },
+    equipment() {
+      return this.dChar.equipment;
+    },
+  },
+};
+</script>

2fb1822d63e8e2718da47e397dde17818bc241f7
thislooksfun <thislooksfun@repbot.org>
2020-02-08 12:44:55 -0600

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "scope-enum": [
+      2,
+      "always",
+      [
+        // Packages
+        "package",
+        // Tests
+        "unit",
+        "e2e",
+        // Other
+        // TBD
+      ],
+    ],
+  },
+};

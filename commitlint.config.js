4715c205d16c977de8735ad2e0067475bcf59c41
thislooksfun <thislooksfun@repbot.org>
2020-02-08 13:13:50 -0600

    ci(commitlint): don't enforce scope for now
    Restricting the scopes is unnecessary for now, and also slows down the commit process. Once we have been using the system for a while we can evaluate which scopes make sense to keep, but for now let's not impose any restrictions.

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,18 +1,20 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "scope-enum": [
-      2,
-      "always",
-      [
-        // Packages
-        "package",
-        // Tests
-        "unit",
-        "e2e",
-        // Other
-        // TBD
-      ],
-    ],
+    // "scope-enum": [
+    //   2,
+    //   "always",
+    //   [
+    //     // Packages
+    //     "package",
+    //     // Tests
+    //     "unit",
+    //     "e2e",
+    //     //
+    //     "mergify",
+    //     // Other
+    //     // TBD
+    //   ],
+    // ],
   },
 };

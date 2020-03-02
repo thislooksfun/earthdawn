6882a17f9dc0ffb6a90ffe66b0e28b85c8973ca4
thislooksfun <thislooksfun@repbot.org>
2020-03-02 16:13:18 -0600

    fix: only apply styles to current level

--- src/components/helper/ValueLabelGroup.vue
+++ src/components/helper/ValueLabelGroup.vue
@@ -45,14 +45,14 @@ export default {
     }
   }
 
-  .label {
+  & > .label {
     position: relative;
     display: block;
     margin: -0.25rem 0 -0.5rem 0;
     color: var(--text-secondary);
     font-size: 0.85rem;
   }
-  .value {
+  & > .value {
     display: block;
     margin-bottom: -0.5rem;
     font-size: 2rem;

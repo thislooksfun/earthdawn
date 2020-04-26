5118a6cd7c01935ab37c0a801de6903aec47fc1f
thislooksfun <thislooksfun@repbot.org>
2020-04-25 19:01:03 -0500

    feat: start work on responsive templates

--- src/charDecorator/decorators/dynamic/spells.js
+++ src/charDecorator/decorators/dynamic/spells.js
@@ -1,13 +1,36 @@
 import spells from "Spells";
+import { evalTemplate } from "@/helper/evaluate";
 // import actionDiceForStep from "@/helper/actionDiceForStep";
 
 export default function(char) {
   for (const spell in char.spells) {
+    const sp = spells[char.discipline.name.toLowerCase()][spell];
+
     char.spells[spell] = {
       // Load the data from the spell
-      ...spells[char.discipline.name.toLowerCase()][spell],
-      // The character just stores the rank of the spell, add that too
-      rank: char.spells[spell],
+      ...sp,
+
+      get casting() {
+        return evalTemplate(sp.casting || "", char);
+      },
+      get range() {
+        return evalTemplate(sp.range || "", char);
+      },
+      get duration() {
+        return evalTemplate(sp.duration || "", char);
+      },
+      get effect() {
+        return evalTemplate(sp.effect || "", char);
+      },
+      get areaOfEffect() {
+        return evalTemplate(sp.areaOfEffect || "", char);
+      },
+      get successLevels() {
+        return sp.successLevels.map(sl => evalTemplate(sl, char));
+      },
+      get extraThreads() {
+        return sp.extraThreads.map(sl => evalTemplate(sl, char));
+      },
     };
   }
 }
